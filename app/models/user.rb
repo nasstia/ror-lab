de108c04bba04cde06c32d1d7a68cf688de462ae
nasstia <nastia.sssverenyak@gmail.com>
2017-10-23 10:45:29 +0300

    only letters in some text-fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :firstname, :lastname, :email, :password, :country, :city, :street, :homenumber, presence: true
   validates :homenumber, numericality: { only_integer: true }
+  validates :firstname, :lastname, :country, :city, :street, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
 end
