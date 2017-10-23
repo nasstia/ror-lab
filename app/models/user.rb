aaf9f3bfaaf4ab3920da7a4d00d674bbdb521f8f
nasstia <nastia.sssverenyak@gmail.com>
2017-10-23 10:37:59 +0300

    homenumber only number

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   validates :firstname, :lastname, :email, :password, :country, :city, :street, :homenumber, presence: true
+  validates :homenumber, numericality: { only_integer: true }
 end
