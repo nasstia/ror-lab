ec68d51d6a3cfe4594d54e9dda3aa9275d544716
nasstia <nastia.sssverenyak@gmail.com>
2017-10-23 10:51:51 +0300

    invalid e-mail validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   validates :homenumber, numericality: { only_integer: true }
   validates :firstname, :lastname, :country, :city, :street, format: { with: /\A[a-zA-Z]+\z/,
     message: "only allows letters" }
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+    message: "invalid e-mail format" }
 end
